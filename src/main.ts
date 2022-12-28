665f5329d719f22bdc34862f16bfdf0eb44e35be
Corentin Cailleaud <corentin.cailleaud@caillef.com>
2022-12-28 21:55:48 +0100

    fix CORS

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000', 'https://voxdream.art'],
-  });
+  // app.enableCors({
+  //   origin: ['http://localhost:3000', 'https://voxdream.art'],
+  // });
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    return next();
   });
   await app.listen(3000);
 }
